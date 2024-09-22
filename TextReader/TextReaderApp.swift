3cdb75309a508618e50636e455f3919dff4b97c6
ddhjy <510893492@qq.com>
2024-09-22 20:08:42 +0800

    init

---
+++ TextReader/TextReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextReaderApp.swift
+//  TextReader
+//
+//  Created by zengkai on 2024/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TextReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
