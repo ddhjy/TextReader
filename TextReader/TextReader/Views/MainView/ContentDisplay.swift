c6ad41b4bf43fb1d5c90666ded484d977ec4e9ca
ddhjy <510893492@qq.com>
2025-12-06 21:05:15 +0800

    refactor: add id and disable animation for content display view

--- TextReader/TextReader/Views/MainView/ContentDisplay.swift
+++ TextReader/TextReader/Views/MainView/ContentDisplay.swift
@@ -14,6 +14,10 @@ struct ContentDisplay: View {
             .padding(.vertical, 12)
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
             .contentShape(Rectangle())
+            .id(viewModel.currentPageIndex)
+            .transaction { transaction in
+                transaction.animation = nil
+            }
             .gesture(
                 LongPressGesture(minimumDuration: 0.3)
                     .onEnded { _ in
